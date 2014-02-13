b691b1c32292df1fb2271e6dc978a17aabb2ede7
Server Admin <admin@fastwebmedia.com>
2014-02-13 16:14:56 +0000

    latest cache changes

--- config/imagefly.php
+++ config/imagefly.php
@@ -9,8 +9,8 @@
 return array
 (
 	'cache_expire'     => 7 * 24 * 60 * 60,
-	'cache_dir'        => 'media/imagecache/',
-	'source_dir'       => MEDIAPATH.'images/',
+	'cache_dir'        => 'media/cache/imagecache/',
+	'source_dir'       => MEDIAPATH,
 	'quality'          => 95,
 	'mimic_source_dir' => FALSE,
 	'frame'            => FALSE,	
